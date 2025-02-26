1770d7efc8e23812ad7343f133d6d2df15d6e9fa
Cristian Olmedo07 <oc0889032025@unab.edu.sv>
2025-02-26 14:04:33 -0600

    se agrega el proyecto

---
+++ enradaDatos.py
@@ -0,0 +1,11 @@
+mensaje = input("Ingrse un texto")
+print(mensaje)
+
+valor = str(mensaje) * int(input("ingres un numero"))
+print(valor)
+
+a1 = input("Ingrese un numero")
+a2 = input("Ingrese un numero")
+suma= int(a1) + int(a2)
+print(suma)
+print(a1 + a2)
